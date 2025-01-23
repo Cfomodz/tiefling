526301c95cb17f7ace9a35910e1980e32bcfd59a
Robert Gerlach <info@robertgerlach.net>
2025-01-23 03:24:24 +0100

    higher mouse sensitivity

--- site/public/js/tiefling/tiefling.js
+++ site/public/js/tiefling/tiefling.js
@@ -504,7 +504,7 @@ export const TieflingView = function (container, image, depthMap, options) {
 
     let mouseXOffset = options.mouseXOffset || 0;
     let focus = options.focus || 0.3;
-    let baseMouseSensitivity = options.mouseSensitivity || 0.2;
+    let baseMouseSensitivity = options.mouseSensitivity || 0.5;
     let mouseSensitivityX = baseMouseSensitivity;
     let mouseSensitivityY = baseMouseSensitivity;
     let devicePixelRatio = options.devicePixelRatio || Math.min(window.devicePixelRatio, 2) || 1;
